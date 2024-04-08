396f73267dc2c299262d2d2cab0228272aa12695
Daniel Angst <dcangst@gmail.com>
2024-04-08 16:05:45 +0200

    formatting

--- R/accFunctions.R
+++ R/accFunctions.R
@@ -6,13 +6,17 @@
 #' @section Output:
 #'   \code{POSIX.ct} timestamp or a vector thereof
 #' @family internal
-.convertXMLtime <- function(string){
+.convertXMLtime <- function(string) {
   options(digits.secs = 6)
   raw_datetime <- str_replace(string, "Z", "")
   raw_datetime <- str_split(string, "T")
-  timestamp <- strptime(lapply(
-    raw_datetime, str_c, collapse = " "),
-    format = "%Y-%m-%d %H:%M:%OS")
+  timestamp <- strptime(
+    lapply(
+      raw_datetime, str_c,
+      collapse = " "
+    ),
+    format = "%Y-%m-%d %H:%M:%OS"
+  )
   return(timestamp)
 }
 
@@ -21,11 +25,11 @@
 #' takes a vector representing a binary number and gives the decimal value
 #' @param x a vector consisting of 0 and 1 representing a binary number
 #' @family internal
-.binToDec <- function(x){
-  if (length(x[x > 1]) > 0){
+.binToDec <- function(x) {
+  if (length(x[x > 1]) > 0) {
     rlang::abort("not a binary number")
   }
-  power <- 2 ^ (rev(seq_along(x)) - 1)
+  power <- 2^(rev(seq_along(x)) - 1)
   return(sum(x * power))
 }
 
@@ -37,7 +41,7 @@
 #' @param x a decimal number
 #' @param l length of the vector
 #' @family internal
-.decToBin <- function(x, l = 7){
+.decToBin <- function(x, l = 7) {
   as.numeric(intToBits(x)[1:l])
 }
 
@@ -48,9 +52,9 @@
 #' @param length length of binary number, should always be 7.
 #' @family accessory functions
 #' @export
-ANSIIfromVector  <- function(x, length = 7){
+ANSIIfromVector <- function(x, length = 7) {
   bin <- rep(0, length)
-  for (i in 1:length(x)){
+  for (i in 1:length(x)) {
     bin[x[i]] <- 1
   }
   dec <- .binToDec(rev(bin))
@@ -69,21 +73,26 @@ ANSIIfromVector  <- function(x, length = 7){
 #' @param nrow Number of rows in labware, defaults to 16 (a 384 well plate)
 #' @family accessory functions
 #' @export
-ANSIIFromWells <- function(wellSelection, ncol = 24, nrow = 16){
+ANSIIFromWells <- function(wellSelection, ncol = 24, nrow = 16) {
   wellLayout <- str_c(
     format(as.hexmode(c(ncol, nrow)),
-      width = 2, upper.case = TRUE),
-      collapse = "")
+      width = 2, upper.case = TRUE
+    ),
+    collapse = ""
+  )
   wellPlate <- ncol * nrow
   nGroup7 <- ceiling(wellPlate / 7)
 
-  plate_wells  <- matrix(1:wellPlate, ncol = ncol, byrow = TRUE)
+  plate_wells <- matrix(1:wellPlate, ncol = ncol, byrow = TRUE)
   plate_group7 <- matrix(rep(1:nGroup7, each = 7)[1:wellPlate],
-    ncol = ncol, nrow = nrow)
+    ncol = ncol, nrow = nrow
+  )
 
-  df <- tibble(well = as.vector(plate_wells),
+  df <- tibble(
+    well = as.vector(plate_wells),
     group7 = as.vector(plate_group7),
-    group7pos = rep(1:7, nGroup7)[1:wellPlate])
+    group7pos = rep(1:7, nGroup7)[1:wellPlate]
+  )
 
   well_df <- df[match(wellSelection, df$well), ]
   well_df[is.na(well_df)] <- 0
@@ -92,11 +101,12 @@ ANSIIFromWells <- function(wellSelection, ncol = 24, nrow = 16){
   used <- well_df %>%
     group_by(group7) %>%
     summarize(ansi = ANSIIfromVector(group7pos))
-  for (i in 1:nGroup7){
+  for (i in 1:nGroup7) {
     bitmap[used$group7[i]] <- used$ansi[i]
   }
   bitmap_string <- str_c(c("\"", wellLayout, bitmap, "\""),
-    collapse = "")
+    collapse = ""
+  )
   return(bitmap_string)
 }
 
@@ -107,11 +117,12 @@ ANSIIFromWells <- function(wellSelection, ncol = 24, nrow = 16){
 #' @param wellSelection well selection string
 #' @family accessory functions
 #' @export
-wellsFromANSII <- function(wellSelection){
+wellsFromANSII <- function(wellSelection) {
   ncol <- as.integer(as.hexmode(str_sub(wellSelection, 2, 3)))
   nrow <- as.integer(as.hexmode(str_sub(wellSelection, 4, 5)))
   groups <- unlist(str_split(str_sub(wellSelection,
-    start = 6, end = -2), ""))
+    start = 6, end = -2
+  ), ""))
   exChars <- c(".", "\\", "|", "(", ")", "[", "{", "^", "$", "*", "+", "?")
   whichSpecial <- groups %in% exChars
   groups[whichSpecial] <- str_c("\\", groups[whichSpecial])
@@ -120,8 +131,9 @@ wellsFromANSII <- function(wellSelection){
     unlist(
       lapply(groups_int, .decToBin)
     )[1:(nrow * ncol)],
-    nrow, ncol)
-  if (sum(wells_matrix) == 0){
+    nrow, ncol
+  )
+  if (sum(wells_matrix) == 0) {
     wells <- 0
   } else {
     wells <- matrix(1:(nrow * ncol), nrow, byrow = TRUE)[wells_matrix == 1]
@@ -134,14 +146,14 @@ wellsFromANSII <- function(wellSelection){
 #' generates the string of volumes needed for the advanced worklist
 #' @param volumes \code{vector("numeric",12L)}
 #' @family internal
-.advVolumeString <- function(volumes, tipMask){
+.advVolumeString <- function(volumes, tipMask) {
   vol_warning <- ""
-  if (length(volumes) == 1){
+  if (length(volumes) == 1) {
     volumes12 <- c(tipMask * volumes, rep(0, 4))
     vol_warning <- "Only one Volume, guessed from tipMask & Volume. Check!"
-  } else if (length(volumes) == 8){
+  } else if (length(volumes) == 8) {
     # check for mismatch between selected tips and supplied volumes
-    if (sum(tipMask * volumes - volumes) < 0){
+    if (sum(tipMask * volumes - volumes) < 0) {
       rlang::abort("tipMask / volumes mismatch!")
     }
     volumes12 <- c(volumes, rep(0, 4))
@@ -151,8 +163,10 @@ wellsFromANSII <- function(wellSelection){
 
   volumes_str <- vector("character", 12L)
   volumes_str[which(volumes12 == 0)] <- "0"
-  volumes_str[which(!volumes12 == 0)] <- paste0("\"",
-    volumes12[which(!volumes12 == 0)], "\"")
+  volumes_str[which(!volumes12 == 0)] <- paste0(
+    "\"",
+    volumes12[which(!volumes12 == 0)], "\""
+  )
   volumes_str <- str_c(volumes_str, collapse = ",")
   return(list(volumes_str, vol_warning))
 }
@@ -163,16 +177,18 @@ wellsFromANSII <- function(wellSelection){
 #' Used in advanced worklist functions
 #' @param RackLabel name of the Rack
 #' @family internal
-getGridSite <- function(RackLabel){
+getGridSite <- function(RackLabel) {
   RackLabel_wt <- gwl$worktable[gwl$worktable$RackLabel == RackLabel, ]
   if (dim(RackLabel_wt)[1] == 1) {
     grid <- RackLabel_wt$grid
     site <- RackLabel_wt$site
-  } else if (dim(RackLabel_wt)[1] < 1){
+  } else if (dim(RackLabel_wt)[1] < 1) {
     print(gwl$worktable$worktable)
-    rlang::abort(paste0("Racklabel: "), RackLabel,
-      " not defined. Define Racklabels using addToWorktable() or manually @ gwl$worktable")
-  } else if (dim(RackLabel_wt)[1] > 1){
+    rlang::abort(
+      paste0("Racklabel: "), RackLabel,
+      " not defined. Define Racklabels using addToWorktable() or manually @ gwl$worktable"
+    )
+  } else if (dim(RackLabel_wt)[1] > 1) {
     print(gwl$worktable$worktable)
     rlang::abort("Duplicate RackLabels? check worktable")
   }
@@ -186,7 +202,7 @@ getGridSite <- function(RackLabel){
 #' @param RackLabel name of the Racklabel
 #' @param direction 1 to add volume, -1 to subtract volume
 #' @family internal
-.updateVolume <- function(volumes, RackLabel, direction = 1){
+.updateVolume <- function(volumes, RackLabel, direction = 1) {
   gwl$worktable[gwl$worktable$RackLabel == RackLabel, ]$Volume <<-
     gwl$worktable[gwl$worktable$RackLabel == RackLabel, ]$Volume +
     direction * sum(volumes)
@@ -198,11 +214,11 @@ getGridSite <- function(RackLabel){
 #' display. Trys to make guesses for column names. Used in \code{\link{read_Infinite_XML}}
 #' @param list a list of vectors
 #' @family internal
-.listToDf <- function(list){
+.listToDf <- function(list) {
   list_lengths <- sapply(list, length)
   df <- data.frame(matrix(nrow = length(list), ncol = max(list_lengths)))
   names(df) <- names(list[[which(list_lengths == max(list_lengths))[1]]])
-  for (i in seq_along(list)){
+  for (i in seq_along(list)) {
     length(list[[i]]) <- max(list_lengths)
     df[i, ] <- list[[i]]
   }
@@ -218,9 +234,9 @@ getGridSite <- function(RackLabel){
 #' @param n_col number of columns in labware
 #' @family general
 #' @export
-rc_to_well <- function(row, col, n_row, n_col){
-  if (is.character(row[1])){
-     row <- match(row, LETTERS[1:n_row])
+rc_to_well <- function(row, col, n_row, n_col) {
+  if (is.character(row[1])) {
+    row <- match(row, LETTERS[1:n_row])
   }
   well <- col + (row - 1) * n_col
   return(well)
@@ -234,14 +250,14 @@ rc_to_well <- function(row, col, n_row, n_col){
 #' @family general
 #' @export
 generateMasks <- function(tip, vol) {
-    tip_mask <- integer(8)
-    vol_mask <- integer(8)
+  tip_mask <- integer(8)
+  vol_mask <- integer(8)
 
-    tip_mask[tip] <- 1
-    vol_mask[tip] <- vol
+  tip_mask[tip] <- 1
+  vol_mask[tip] <- vol
 
-    masks <- lst(tip_mask, vol_mask)
-    return(masks)
+  masks <- lst(tip_mask, vol_mask)
+  return(masks)
 }
 
 #' Shortest path for pin deactivation
@@ -256,7 +272,8 @@ shortestPath <- function(pins, method = "two_opt") {
   well_col <- pins - well_row * 24 - 1
 
   data <- dist(t(matrix(c(well_row, well_col), nrow = 2, byrow = T)),
-    diag = T, upper = T)
+    diag = T, upper = T
+  )
 
   order <- TSP::solve_TSP(TSP::ATSP(data), method = method)
   return(pins[order])
@@ -270,7 +287,7 @@ shortestPath <- function(pins, method = "two_opt") {
 #' @param arg_types One of NULL or a cols specification. See vignette("column-types", package = "readr") for more details.
 #' @family accessory
 #' @export
-parse_args <- function(arg_names, arg_types = NULL){
+parse_args <- function(arg_names, arg_types = NULL) {
   args <- commandArgs(trailingOnly = TRUE)
   if (length(args) == 0) {
     args <- as.character(rep(1, str_count(arg_types)))
@@ -278,7 +295,7 @@ parse_args <- function(arg_names, arg_types = NULL){
   }
   length(args) <- length(arg_names)
   names(args) <- arg_names
-  if (is.character(arg_types)){
+  if (is.character(arg_types)) {
     arg_types <- readr:::as.col_spec(arg_types)
   }
   args_df <- data.frame(t(args), stringsAsFactors = FALSE)
@@ -305,7 +322,8 @@ binToInt <- function(x) {
   for (i in x) {
     out <- c(
       out,
-      sum(2 ^ (which(rev(unlist(strsplit(as.character(i), "")) == 1)) - 1)))
+      sum(2^(which(rev(unlist(strsplit(as.character(i), "")) == 1)) - 1))
+    )
   }
   out
 }
